e766bc34285b7c0bf69a7449fbe3630d040d01d6
YassineKassem <yassinekassem33@gmail.com>
2022-03-12 14:57:33 +0100

    detail page

--- client/lib/welcome_etudiant/stage_list.dart
+++ client/lib/welcome_etudiant/stage_list.dart
@@ -1,25 +1,32 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pfe/stage_detail.dart';
 import 'package:pfe/welcome_etudiant/stage.dart';
 import 'package:pfe/welcome_etudiant/stage_item.dart';
 
 class JobList extends StatelessWidget {
-  final jobList= Job.generateJobs();
+  final jobList = Job.generateJobs();
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 25),
       height: 160,
-      child: ListView.separated
-      (
-        padding: EdgeInsets.symmetric(horizontal: 25),
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) => JobItem(jobList[index]),
-        separatorBuilder: (_, index) => SizedBox
-        (
-          width: 15,
-        ),
-        itemCount: jobList.length
-       ),
+      child: ListView.separated(
+          padding: EdgeInsets.symmetric(horizontal: 25),
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) => GestureDetector(
+              onTap: () {
+                showModalBottomSheet(
+                    backgroundColor: Colors.transparent,
+                    isScrollControlled: true,
+                    context: context,
+                    builder: (context) => JobDetail(jobList[index]));
+              },
+              child: JobItem(jobList[index])),
+          separatorBuilder: (_, index) => SizedBox(
+                width: 15,
+              ),
+          itemCount: jobList.length),
     );
   }
 }
